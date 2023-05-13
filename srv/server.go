51a8b86dd30a98f7fd79ab4e66fa845bba36c5b9
benoitAD <benoit.augerdubois@gmail.com>
2023-05-13 15:29:37 +0200

    feat: choix des personnages côté serveur

--- srv/server.go
+++ srv/server.go
@@ -76,6 +76,54 @@ func notifyClient(client *Client, gameState *int) {
 	}
 }
 
+// permet de lire un message envoyé par un client
+func readMessage(conn *net.Conn) (string, error) {
+	buffer := make([]byte, 1024)
+	n, err := (*conn).Read(buffer)
+	if err != nil {
+		return "", err
+	}
+	return string(buffer[:n]), nil
+}
+
+func waitForAllClientsToChooseCharacter(clients []Client) {
+	channels := make([]chan bool, len(clients))
+
+	// Créer un canal pour chaque client
+	for i := 0; i < len(clients); i++ {
+		channels[i] = make(chan bool)
+	}
+
+	// Lancer une goroutine pour chaque client qui attend un message booléen
+	for i, client := range clients {
+		go func(i int, client Client) {
+			for {
+				message, err := readMessage(client.conn)
+				if err != nil {
+					log.Println("error reading message from client ", client.id, err)
+					continue
+				}
+				if message == "true" {
+					channels[i] <- true
+					break
+				}
+			}
+		}(i, client)
+	}
+
+	// Attendre que tous les canaux aient reçu une valeur true
+	for _, ch := range channels {
+		<-ch
+	}
+}
+
+func setState(gameState *int, newState int, clients []Client) {
+	*gameState = newState
+	for _, client := range clients {
+		notifyClient(&client, gameState)
+	}
+}
+
 func main() {
 	gameState := StateWelcomeScreen
 
@@ -91,7 +139,7 @@ func main() {
 	// Fermer le listener quand le programme se termine
 	defer listener.Close()
 
-	for len(clients) < 2 {
+	for len(clients) < 4 {
 		conn, err := listener.Accept()
 
 		clients = append(clients, Client{&conn, fmt.Sprint("client-", len(clients))})
@@ -107,13 +155,14 @@ func main() {
 
 	log.Println("Tous les clients sont connectés")
 
-	gameState++
-
+	setState(&gameState, StateChooseRunner, clients)
 	log.Println("Notifier les clients: ", gameState)
 
-	for _, client := range clients {
-		notifyClient(&client, &gameState)
-	}
+	// fonction qui attend de recevoir un message de chaque client pour passer a l'état suivant
+	waitForAllClientsToChooseCharacter(clients) // appel synchrone qui bloque le programme
+
+	log.Println("Tous les clients ont choisit leur personnage")
+	setState(&gameState, StateLaunchRun, clients)
 
 	for {
 		time.Sleep(1 * time.Second)
